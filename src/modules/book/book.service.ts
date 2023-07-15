176fb03392740d6bbe6e5defb8e0d885d5c98a41
miroswd <altamirsantos93@hotmail.com>
2023-07-15 14:09:24 -0300

    feat: create crud with nestjs and prisma

---
+++ src/modules/book/book.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@nestjs/common';
+import { BookDTO } from './book.dto';
+import { PrismaService } from 'src/database/PrismaService';
+
+@Injectable()
+export class BookService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: BookDTO) {
+    const bookExists = await this.prisma.book.findFirst({
+      where: {
+        bar_code: data.bar_code,
+      },
+    });
+
+    if (bookExists) {
+      throw new Error('Book already exists');
+    }
+
+    const book = await this.prisma.book.create({
+      data,
+    });
+
+    return book;
+  }
+
+  async findAll() {
+    return await this.prisma.book.findMany();
+  }
+
+  async update(id: string, data: BookDTO) {
+    const bookExists = await this.prisma.book.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!bookExists) {
+      throw new Error('Book does not exists');
+    }
+
+    return await this.prisma.book.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    const bookExists = await this.prisma.book.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!bookExists) {
+      throw new Error('Book does not exists');
+    }
+
+    await this.prisma.book.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
